0e013b0e0985dee8f98a9170246a6a4ee625c392
Ivans Aleskevics <vanjaaleskevic@mail.ru>
2021-04-20 16:55:45 +0300

    popup funkcija js un vairak css

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("JavaScripit is working!")
+
+document.getElementById('pievienotSarakstam').addEventListener('click', () => {
+    console.log("Poga nospiesta!");
+
+    let popUp = document.getElementById('popUp');
+    
+    popUp.style.display = 'block'
+})
